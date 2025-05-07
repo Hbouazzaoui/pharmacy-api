eaa0121871187d84ad95ec64ec0dc5eb6e1c95de
Hbouazzaoui <bouazzaouiihamouda@gmail.com>
2025-05-07 12:53:35 +0100

    Add ProductRepository interface for JPA CRUD operations

---
+++ src/main/java/com/example/pharmacie/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.example.pharmacie.repositories;
+
+import com.example.pharmacie.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
