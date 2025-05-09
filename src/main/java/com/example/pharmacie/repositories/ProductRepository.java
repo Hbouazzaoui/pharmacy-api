8bf710e77ffb5176acf69f59952dffeb654cd769
Hbouazzaoui <bouazzaouiihamouda@gmail.com>
2025-05-09 15:58:43 +0100

    Fix and Push Project Complate

--- src/main/java/com/example/pharmacie/repositories/ProductRepository.java
+++ src/main/java/com/example/pharmacie/repositories/ProductRepository.java
@@ -1,6 +1,6 @@
 package com.example.pharmacie.repositories;
 
-import com.example.pharmacie.model.Product;
+import com.example.pharmacie.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
