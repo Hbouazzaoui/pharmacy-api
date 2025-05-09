7bc7ac147bedcccd7765f829bc44ff379872c75f
Hbouazzaoui <bouazzaouiihamouda@gmail.com>
2025-05-09 15:51:47 +0100

    Implement POST (add product) and GET PUT (update product) and Implement DELETE (remove product)

--- src/main/java/com/example/pharmacie/controller/ProductController.java
+++ src/main/java/com/example/pharmacie/controller/ProductController.java
@@ -1,6 +1,34 @@
 package com.example.pharmacie.controller;
 
+import com.example.pharmacie.entity.Product;
+import com.example.pharmacie.repositories.ProductRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
 public class ProductController {
+    private final ProductRepository productRepository;
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+    @PostMapping("/product")
+    public Product saveProduct( @RequestBody Product product){
+        return productRepository.save(product);
+    }
+    @GetMapping("/products")
+    public List<Product> getProduct(){
+        return productRepository.findAll();
+    }
+    @DeleteMapping("/product/{id}")
+    public void deleteProduct(@PathVariable long id){
+        productRepository.deleteById(id);
+    }
 
+    @PutMapping
+    public Product updateProduct(@RequestBody Product product){
+        return productRepository.save(product);
 
+    }
 }
